9f5920846fe41c44e9afb3e793c7dd7916e4205d
laxy <laxyrajput@gmail.com>
2024-06-28 16:44:31 +0530

    Adding projection

--- src/main/java/product/repository/ProductRepository.java
+++ src/main/java/product/repository/ProductRepository.java
@@ -1,14 +1,18 @@
 package product.repository;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import product.dtos.ProductDto;
 import product.models.Product;
+import product.projection.ProductWithTitleAndDescription;
 
-import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+
+    //HQL
+    @Query("select p.title as title, p.description as description from product p where p.id = :id")
+    ProductWithTitleAndDescription fetchTitleAndDescription(@Param("id") Long id);
 }
